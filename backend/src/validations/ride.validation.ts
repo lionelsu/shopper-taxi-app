23fa8a9adbc686935fc4b8d972d3a59d494ebf78
Lionel Miranda <bibliothecalionel@gmail.com>
2024-11-25 14:10:42 +0000

    fix: make helpers message print correct text

--- backend/src/validations/ride.validation.ts
+++ backend/src/validations/ride.validation.ts
@@ -1,4 +1,4 @@
-import Joi from "joi";
+import Joi, { custom } from "joi";
 
 export const estimateRideSchema = Joi.object({
   origin: Joi.string().trim().required().messages({
@@ -14,7 +14,7 @@ export const estimateRideSchema = Joi.object({
       "any.required": "O ID do usuário é obrigatório."
   })}).custom((value, helpers) => {
   if (value.origin === value.destination) {
-      return helpers.error("any.custom", { message: "Os endereços de origem e destino não podem ser iguais." });
+    return helpers.message({ custom: "Os endereços de origem e destino não podem ser iguais." });
   }
   return value;
 });
