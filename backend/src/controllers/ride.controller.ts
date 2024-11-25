8115564299fb22f42780616fa98a6f0e341025f3
Lionel Miranda <bibliothecalionel@gmail.com>
2024-11-25 14:00:43 +0000

    feat: Add POST /ride/estimate endpoint with Google Maps API integration

--- backend/src/controllers/ride.controller.ts
+++ backend/src/controllers/ride.controller.ts
@@ -1,33 +1,41 @@
 import { NextFunction, Request, Response } from "express";
 import { estimateRideSchema } from "../validations/ride.validation";
-import { getDistanceFromGoogleMaps } from "../services/googleMaps.service";
+import { getDistanceFromGoogleMaps, getCoordinates } from "../services/googleMaps.service";
 import { getAvailableDrivers } from "../services/drivers";
 
 export const estimateRide = async (req: Request, res: Response, next: NextFunction): Promise<any> => {
   const { origin, destination, customer_id } = req.body;
 
   const { error } = estimateRideSchema.validate({ origin, destination, userId: customer_id });
+
   if (error) {
+      console.log(error.details)
       return res.status(400).json({ error_code: "INVALID_DATA", error_description: error.details[0].message });
   }
 
   try {
+      const originCoordinates = await getCoordinates(origin)
+      const destinationCoordinates = await getCoordinates(destination)
+
       const routeResponse = await getDistanceFromGoogleMaps(origin, destination);
 
-      const { distance, duration, start_location, end_location } = routeResponse;
+      const { distance, duration } = routeResponse;
 
-      const availableDrivers = getAvailableDrivers(distance);
+      const distanceInKm = distance.value / 1000
+      const durationInText = duration.text
+      
+      const availableDrivers = getAvailableDrivers(distanceInKm);
 
       return res.status(200).json({
-        origin: { latitude: start_location, longitude: start_location },
-        destination: { latitude: end_location, longitude: end_location },
-        distance,
-        duration,
+        origin: { latitude: originCoordinates.lat, longitude: originCoordinates.lng },
+        destination: { latitude: destinationCoordinates.lat, longitude: destinationCoordinates.lng },
+        distance: distanceInKm,
+        duration: durationInText,
         options: availableDrivers,
         routeResponse
       });
-  } catch (err) {
-      console.error(err);
+  } catch (error) {
+      console.error(error);
       return res.status(500).json({ error: "Erro ao estimar a viagem." });
   }
 };
--- backend/src/services/googleMaps.service.ts
+++ backend/src/services/googleMaps.service.ts
@@ -2,6 +2,28 @@ import axios from 'axios';
 
 const GOOGLE_API_KEY = process.env.GOOGLE_API_KEY || '';
 
+export const getCoordinates = async (address: string) => {
+    const geocodingUrl = `https://maps.googleapis.com/maps/api/geocode/json?address=${encodeURIComponent(
+      address
+    )}&key=${GOOGLE_API_KEY}`;
+  
+    try {
+      const response = await axios.get(geocodingUrl);
+      const data = response.data;
+  
+      if (data.status !== "OK" || !data.results[0]) {
+        throw new Error("Error fetching coordinates from Google Maps API");
+      }
+  
+      const location = data.results[0].geometry.location;
+      return location;
+    } catch (error) {
+      console.error("Geocoding API Error:", error);
+      throw new Error("Failed to fetch coordinates");
+    }
+  };
+  
+
 export const getDistanceFromGoogleMaps = async (origin: string, destination: string) => {
     
     const googleMapsUrl = `https://maps.googleapis.com/maps/api/distancematrix/json?origins=${encodeURIComponent(
@@ -10,7 +32,6 @@ export const getDistanceFromGoogleMaps = async (origin: string, destination: str
     
     return axios.get(googleMapsUrl).then((response) => {
         const data = response.data;
-        console.log(data.rows[0].elements[0]);
 
         if (data.status !== "OK" || data.rows[0].elements[0].status !== "OK") {
             throw new Error("Error fetching distance from Google Maps API");
@@ -18,7 +39,7 @@ export const getDistanceFromGoogleMaps = async (origin: string, destination: str
 
         return data.rows[0].elements[0];
     }).catch((error) => {
-        console.error("Error fetching distance:", error.message || error);
+        console.error("Error fetching distance:", error);
         throw new Error("Error fetching distance from Google Maps API");
     });
 };
